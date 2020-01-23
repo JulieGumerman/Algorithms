bc9138a7f97d117b2e8b91922bc78f4c5079062b
bodhibrdr <julie.gumerman@gmail.com>
2020-01-23 14:24:51 -0700

    the logic works...

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -6,10 +6,26 @@ import itertools
 def rock_paper_scissors(n):
   rps = ['rock', 'paper', 'scissors']
   rpsn = rps * n
+  combinations = []
 
-  return rpsn
-
-
+  if n == 0:
+    return [[]]
+  elif n == 1:
+    for x in rpsn:
+      new = [x]
+      combinations.append(new)
+  elif n == 2:
+    for x in rpsn:
+      for y in rpsn:
+        new = [x, y]
+        combinations.append(new)
+  elif n == 3:
+    for x in rpsn:
+      for y in rpsn:
+        for z in rpsn:
+          new = [x, y, z]
+          combinations.append(new)
+  return combinations
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
